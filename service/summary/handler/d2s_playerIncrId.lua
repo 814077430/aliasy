70205064edf092eba939773eb2e5354e220afb25
aliasy <11485585+liang-bowei@user.noreply.gitee.com>
2022-10-10 12:35:40 +0000

    base framework

---
+++ service/summary/handler/d2s_playerIncrId.lua
@@ -0,0 +1,8 @@
+local skynet = require "skynet"
+local cmds = require "cmds"
+local err = require "error"
+local const = require "const"
+
+function cmds.d2s_playerIncrId(fd, msg)
+    
+end
---
+++ service/summary/handler/d2s_start.lua
@@ -0,0 +1,8 @@
+local skynet = require "skynet"
+local cmds = require "cmds"
+local err = require "error"
+local const = require "const"
+
+function cmds.d2s_start(fd, msg)
+    
+end
