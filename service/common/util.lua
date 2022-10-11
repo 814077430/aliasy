3f3785d9a6c52f8a711e31f591bb238e2e929039
aliasy <11485585+liang-bowei@user.noreply.gitee.com>
2022-10-11 11:13:06 +0000

    base framework

---
+++ service/common/util.lua
@@ -0,0 +1,60 @@
+local skynet = require "skynet"
+local string = require "string"
+
+local util = {}
+ 
+function getByte(data, flag)
+    local array = {}
+    local lens = string.len(data)
+    if flag == false then
+        for i = 1, lens do
+            array[i] = string.byte(data, i)
+        end
+        return array
+    else
+        for i=1, lens do
+            array[i-1] = string.byte(data, i)
+        end
+    end
+    return array, lens
+end
+ 
+function getChars(bytes)
+    local array = {}
+    for key, val in pairs(bytes) do
+        array[key] = string.char(val)
+    end
+    return array
+end
+
+function util.split(str, reps)
+    local resultStrList = {}
+    string.gsub(str, '[^'..reps..']+', function(w)
+        table.insert(resultStrList, w)
+    end)
+
+    return resultStrList
+end
+ 
+function util.encryptData(data, keys)
+    local result = ""
+    local dataArr = getByte(data, false)
+    local keyArr, keyLen = getByte(keys, true)
+    for index, value in pairs(dataArr) do
+        result = result.."@"..tostring((0xFF and value) + (0xFF and keyArr[(index-1) % keyLen]))
+    end
+    return result
+end
+ 
+function util.decryptData(data, keys)
+    local result = ""
+    local dataArr = util.split(data, '@')
+    local keyArr,keyLen = getByte(keys, true)
+    for index, value in pairs(dataArr) do
+          bytes =  tonumber(value) - (0xFF and keyArr[(index-1) % keyLen])
+          result = result..string.char(bytes)
+    end
+    return result
+end
+
+return util
--- service/login/handler/c2s_login.lua
+++ service/login/handler/c2s_login.lua
@@ -3,9 +3,10 @@ local cmds = require "cmds"
 local con = require "connect"
 local err = require "error"
 local const = require "const"
+local util = require "util"
 
 function cmds.c2s_login(fd, msg)
-	--notice other
+	--notice game
 	msg.fd = fd
 	skynet.send(const.Game, "lua", "l2g_login", msg)
 
