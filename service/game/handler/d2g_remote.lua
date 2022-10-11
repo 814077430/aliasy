5779ca76dd14fa39c27f1478ff5860b06c089524
aliasy <11485585+liang-bowei@user.noreply.gitee.com>
2022-10-11 02:12:51 +0000

    base framework

---
+++ service/game/handler/d2g_remote.lua
@@ -0,0 +1,12 @@
+local skynet = require "skynet"
+local cmds = require "cmds"
+local con = require "connect"
+local err = require "error"
+
+function cmds.d2g_playerIncrId()
+    
+end
+
+function cmds.d2g_start()
+
+end
---
+++ service/world/handler/l2w_remote.lua
@@ -0,0 +1,10 @@
+local skynet = require "skynet"
+local cmds = require "cmds"
+local con = require "connect"
+local err = require "error"
+
+function cmds.l2w_login(msg)
+    local uid = msg.uid
+    local fd = msg.fd
+    con.addCon(uid, fd);
+end
