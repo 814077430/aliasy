b54a249414f2cda3d67ab68a34367bbfd2d3e51a
aliasy <11485585+liang-bowei@user.noreply.gitee.com>
2022-10-12 08:11:24 +0000

    base framework

--- sql/initTable.sql
+++ sql/initTable.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS t_general;
 CREATE TABLE IF NOT EXISTS t_general (
   `id` int(20) unsigned NOT NULL AUTO_INCREMENT,
   `playerIncrId` int(20) unsigned NOT NULL DEFAULT '0',
-  `unionIncrID` int(20) unsigned NOT NULL DEFAULT '0',
+  `unionIncrId` int(20) unsigned NOT NULL DEFAULT '0',
   `serverStartTime` bigint(20) unsigned NOT NULL DEFAULT '0',
   `serverStopTime` bigint(20) unsigned NOT NULL DEFAULT '0',
 
@@ -18,5 +18,16 @@ CREATE TABLE IF NOT EXISTS `t_user` (
   
   PRIMARY KEY (`id`),
   UNIQUE KEY `INDEX_PALYER_GUID` (`uid`)
+  
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+DROP TABLE IF EXISTS t_union;
+CREATE TABLE IF NOT EXISTS `t_union` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `unid` int(20) unsigned NOT NULL DEFAULT '0',
+  `base` mediumblob,
+  
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `INDEX_UNION_GUID` (`unid`)
   
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
